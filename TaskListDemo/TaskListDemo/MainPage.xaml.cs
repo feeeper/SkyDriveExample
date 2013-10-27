06927be0553f94b5c5f64277f86f338efbca004b
Andrey Shirobokov <shirobokov.andrey@gmail.com>
2013-10-28 01:10:21 +0400

    initial

---
+++ TaskListDemo/TaskListDemo/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using TaskListDemo.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TaskListDemo {
+  public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged {
+    TaskDataContext db = new TaskDataContext();
+
+    private ObservableCollection<TaskModel> _tasks1;
+    public ObservableCollection<TaskModel> TaskList {
+      get { return _tasks1; }
+      set {
+        if (_tasks1 == value) return;
+        _tasks1 = value;
+        NotifyPropertyChanged("TaskList");
+      }
+    }
+    // Constructor
+    public MainPage() {
+      InitializeComponent();
+      Init();
+    }
+
+    private void Init() {
+      if (!db.DatabaseExists()) {
+        db.CreateDatabase();
+      }
+      if (TaskList == null) {
+        TaskList = new ObservableCollection<TaskModel>(from task in db.Tasks select task);
+      }
+      Tasks.DataContext = this;
+    }
+
+    private void NewTask_Click(object sender, RoutedEventArgs e) {
+      TaskModel task = new TaskModel();
+      task.Title = Guid.NewGuid().ToString();
+      db.Tasks.InsertOnSubmit(task);
+      db.SubmitChanges();
+      TaskList = new ObservableCollection<TaskModel>(from t in db.Tasks select t);
+    }
+
+    #region "Реализация интерфейсов"
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged(string propertyName) {
+      if (PropertyChanged != null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+
+    public event PropertyChangingEventHandler PropertyChanging;
+
+    private void NotifyPropertyChanging(string propertyName) {
+      if (PropertyChanging != null) {
+        PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+      }
+    }
+    #endregion
+  }
+}
---
+++ TaskListDemo/TaskListDemo/TaskModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Data.Linq.Mapping;
+using System.ComponentModel;
+
+namespace TaskListDemo.Data {
+  [Table]
+  public class TaskModel : INotifyPropertyChanged, INotifyPropertyChanging {
+    [Column(IsPrimaryKey = true,
+      IsDbGenerated = true,
+      DbType = "INT NOT NULL Identity",
+      CanBeNull = false,
+      AutoSync = AutoSync.OnInsert)]
+    public int Id {
+      get { 
+        return _id; 
+      }
+      set {
+        if (_id != value) {
+          NotifyPropertyChanging("Id");
+          _id = value;
+          NotifyPropertyChanged("Id");
+        }
+      }
+    }
+    private int _id;
+
+    [Column]
+    public string Title {
+      get {
+        return _title;
+      }
+      set {
+        if (_title != value) {
+          NotifyPropertyChanging("Title");
+          _title = value;
+          NotifyPropertyChanged("Title");
+        }
+      }
+    }
+    private string _title;
+
+    #region "Реализация интерфейсов"
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged(string propertyName) {
+      if (PropertyChanged != null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+
+    public event PropertyChangingEventHandler PropertyChanging;
+
+    private void NotifyPropertyChanging(string propertyName) {
+      if (PropertyChanging != null) {
+        PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+      }
+    }
+    #endregion
+  }
+}
